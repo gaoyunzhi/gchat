f00954f178a2cc2aa15d2dc5079d449e50d1a527
Yunzhi Gao <gaoyunzhi@gmail.com>
2013-10-05 19:44:28 -0700

    add utf compatibility

---
+++ chat.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+from secret import USER, PD
+import sleekxmpp, logging, sys
+logging.basicConfig()
+sys.setdefaultencoding('utf8') #@UndefinedVariable
+    
+class EchoBot : 
+    def __init__(self, jid, password) : 
+        self.xmpp = sleekxmpp.ClientXMPP(jid, password) 
+        self.xmpp.add_event_handler("session_start", self.handleXMPPConnected) 
+        self.xmpp.add_event_handler("message", self.handleIncomingMessage) 
+        
+    def run(self) : 
+        self.xmpp.connect() 
+        self.xmpp.process(threaded=True) 
+        
+    def handleXMPPConnected(self, event): 
+        self.xmpp.sendPresence()
+        
+    def handleIncomingMessage(self, message):
+        print dict(message)
+        self.xmpp.sendMessage(message["from"], message["body"]) 
+        
+if __name__ == '__main__':
+    bot = EchoBot(USER, PD)
+    bot.run()
